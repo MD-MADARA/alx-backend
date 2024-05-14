03adb11f3cf6317b1a69f4820035f74e4e4a7aa8
MD-MADARA <mouadotaku9421@gmail.com>
2024-05-14 13:40:14 +0100

    lifo and fifo

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" BasicCache
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO caching system
+    """
+    def put(self, key, item):
+        """ assign to cache_data the item value for the key key
+        """
+        self.cache_data[key] = item
+        if key and item:
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discarded = list(self.cache_data.keys())[-1]
+                del self.cache_data[discarded]
+                print("DISCARD:", discarded)
+
+    def get(self, key):
+        """ return the value in cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
